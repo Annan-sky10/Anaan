03c68873f29640a37718b4fc92301749f98ac1d7
Annan-sky10 <85247598+Annan-sky10@users.noreply.github.com>
2021-06-03 21:43:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+      const burger = document.querySelector('.burger');
+      const header = document.querySelector('.nav-links');
+      const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener("click", function() {
+        header.classList.toggle("nav-active");
+   
+});
+
+
